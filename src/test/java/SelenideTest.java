3a458878a748694e12c8e0b4ed916b4c66d4feff
Yana Bialkova <yana.bialkova@gmail.com>
2022-08-23 16:30:47 +0200

    scr

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -3,24 +3,36 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Configuration.holdBrowserOpen;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
 
 import java.lang.module.Configuration;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SelenideTest {
 
-    WebDriver driver;
 
     @BeforeAll
     static void setUpAll () {
         WebDriverManager.chromedriver().setup();
     }
 
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999/");
+        holdBrowserOpen = true;
+    }
+
+    private void open(String s) {
+    }
+
     @AfterEach
-    void teardown() {
-        driver.quit();
-        driver = null;
+    void memoryClear() {
+        clearBrowserCookies();
+        clearBrowserLocalStorage();
     }
 
     @Test
