510939ba1a4568bae747ed81a32563c63cbcc54c
Yana Bialkova <yana.bialkova@gmail.com>
2022-08-23 17:48:58 +0200

    Corrected Test

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -3,17 +3,24 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Configuration.holdBrowserOpen;
+import static com.codeborne.selenide.Selectors.byCssSelector;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-import java.lang.module.Configuration;
-import static org.junit.jupiter.api.Assertions.*;
-
 public class SelenideTest {
 
+    public static String getLocalDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy", new Locale("ru")));
+    }
 
     @BeforeAll
     static void setUpAll () {
@@ -26,9 +33,6 @@ public class SelenideTest {
         holdBrowserOpen = true;
     }
 
-    private void open(String s) {
-    }
-
     @AfterEach
     void memoryClear() {
         clearBrowserCookies();
@@ -37,5 +41,15 @@ public class SelenideTest {
 
     @Test
     void shouldTest () {
+        String date = getLocalDate(2);
+        $("[data-test-id ='city'] input").setValue("Москва");
+        $("[data-test-id ='date'] input").setValue(date);
+        $("[data-test-id ='name'] input").setValue("Яна Петрова");
+        $("[data-test-id ='phone'] input").setValue("+79999999999");
+        $("[data-test-id ='agreement']").click();
+        $$(By.className("button__text")).last().click();
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(10));
+        $("[data-test-id=notification]").$x(".//div[@class='notification__title']").should(text("Успешно!"));
+        $("[data-test-id=notification]").$x(".//div[@class='notification__content']").should(text("Встреча успешно забронирована на " + date));
     }
 }
